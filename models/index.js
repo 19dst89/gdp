41eb1bb4de064fbce5b8c2bb9e4ccaefb66db664
Daniel Thompson <19dst89@gmail.com>
2017-02-17 11:35:27 -0800

    heroku db fix

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/gdp");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/gdp" );
 
 var Disaster = require('./disaster');
 

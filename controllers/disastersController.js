9e8f09ef07fabfc292067eda23159faad2f3a049
Daniel Thompson <19dst89@gmail.com>
2017-02-17 11:16:17 -0800

    testing heroku

--- controllers/disastersController.js
+++ controllers/disastersController.js
@@ -8,34 +8,34 @@ function index(req, res) {
 };
 
 // POST /api/disasters
-function create(req, res) {
-  db.Disaster.create(req.body, function(err, newDisaster){
-    if (err){
-      console.log(err)
-    }
-    res.json(newDisaster)
-  });
-  db.Disaster.save(function(err, disaster){
-    if (err){
-      console.log(err)
-    }
-    console.log("saved")
-  })
-}
+// function create(req, res) {
+//   db.Disaster.create(req.body, function(err, newDisaster){
+//     if (err){
+//       console.log(err)
+//     }
+//     res.json(newDisaster)
+//   });
+//   db.Disaster.save(function(err, disaster){
+//     if (err){
+//       console.log(err)
+//     }
+//     console.log("saved")
+//   })
+// }
 
 // GET /api/disasters/:name
-function show(req, res) {
-  db.Disaster.findOne(req.params.name, function(err, oneDisaster){
-    res.json(oneDisaster);
-  });
-}
+// function show(req, res) {
+//   db.Disaster.findOne(req.params.name, function(err, oneDisaster){
+//     res.json(oneDisaster);
+//   });
+// }
 
 // REMOVE /api/disasters/:name
-function destroy(req, res) {
-  db.Disaster.findByOneAndRemove(req.params.name, function(err, disasterToDelete){
-    res.json(disasterToDelete);
-  });
-}
+// function destroy(req, res) {
+//   db.Disaster.findByOneAndRemove(req.params.name, function(err, disasterToDelete){
+//     res.json(disasterToDelete);
+//   });
+// }
 
 // UPDATE /api/disasters/:id
 // function update(req, res) {
@@ -47,6 +47,6 @@ module.exports = {
   index: index,
   create: create,
   show: show,
-  destroy: destroy,
+  destroy: destroy
   // update: update
 };

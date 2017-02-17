7ae90343064004807cfca99e1526fb0df38393d0
kkuo57 <kkuo507@gmail.com>
2017-02-17 15:41:43 -0800

    add more earthquakes to seed.js

--- seed.js
+++ seed.js
@@ -4,72 +4,82 @@ var disasterList = [
   {
     date: "October 17, 1989",
     name: "1989 Loma Prieta",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 63,
-    dmgCost: 6000000000,
-    location: "San Francisco"
+    dmgCost: 6000000000
   },
   {
     date: "March 22, 1957",
-    name: "1957 San Francisco earthquake",
+    name: "1957 San Francisco Earthquake",
+    location: "San Francisco",
+    type: "Earthquake"
     deathToll: 1,
-    dmgCost: 1000000,
-    location: "San Francisco"
+    dmgCost: 1000000
   },
   {
     date: "October 21, 1868",
-    name: "1868 Hayward earthquake",
+    name: "1868 Hayward Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 30,
-    dmgCost: 350000000,
-    location: "San Francisco"
+    dmgCost: 350000000
   },
   {
     date: "June 1838",
-    name: "1838 San Andreas earthquake",
+    name: "1838 San Andreas Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 0,
-    dmgCost: 0,
-    location: "San Francisco"
+    dmgCost: 0
   },
   {
     date: "April 24, 1984",
-    name: "1984 Morgan Hill earthquake",
+    name: "1984 Morgan Hill Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 0,
-    dmgCost: 8000000,
-    location: "San Francisco"
+    dmgCost: 8000000
   },
   {
     date: "August 6, 1979",
-    name: "1979 Coyote Lake earthquake",
+    name: "1979 Coyote Lake Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 0,
-    dmgCost: 500000,
-    location: "San Francisco"
+    dmgCost: 500000
   },
   {
     date: "October 1, 1969",
-    name: "1969 Santa Rosa earthquake",
+    name: "1969 Santa Rosa Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 1,
-    dmgCost: 8350000,
-    location: "San Francisco"
+    dmgCost: 8350000
   },
   {
     date: "August 24, 2014",
-    name: "South",
+    name: "2014 South Napa Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 1,
-    dmgCost: 1000000000,
-    location: "San Francisco"
+    dmgCost: 1000000000
   },
   {
     date: "September 3, 2000",
-    name: "2000",
+    name: "2000 Yountville Earthquake",
+    location: "San Francisco",
+    type: "Earthquake",
     deathToll: 0,
-    dmgCost: 50000000,
-    location: "San Francisco"
+    dmgCost: 50000000
   },
   {
-    date: "TEST DATE",
-    name: "2000",
-    deathToll: 0,
-    dmgCost: 50000000,
-    location: "NOT HERE"
+    date: "October 1, 1969",
+    name: "1969 Santa Rosa Earthquakes",
+    location: "San Francisco",
+    type: "Earthquake",
+    deathToll: 1,
+    dmgCost: 870000000
   }
 ];
 
